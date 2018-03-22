3fd87c06363090e13ab7a0420541809b7fd80c66
eco <ujoychou@live.com>
2018-03-22 17:14:38 +0800

    support throw error when cast message handler.

--- net/AsyncHandlerMap.h
+++ net/AsyncHandlerMap.h
@@ -39,25 +39,53 @@ public:
 		return req_id;
 	}
 
+	// has async message handler.
+	inline bool has_async(IN const uint32_t req_id)
+	{
+		return m_async_map.has(req_id);
+	}
+
+	// pop async message handler.(get and delete)
+	inline MessageHandler::ptr pop_async(
+		IN const uint32_t req_id, 
+		IN const bool last = true)
+	{
+		int eid = 0;
+		if (last) return m_async_map.pop(req_id, eid);
+		MessageHandler::ptr handler;
+		m_async_map.find(handler, req_id);
+		return handler;
+	}
+
+	// pop async message handler.(get and delete)
+	inline MessageHandler::ptr pop_async(
+		IN const uint32_t req_id,
+		IN const char* name, 
+		IN const bool last = true)
+	{
+		int eid = 0;
+		MessageHandler::ptr handler;
+		if (last)
+			handler = m_async_map.pop(req_id, eid);
+		else 
+			m_async_map.find(handler, req_id);
+
+		if (handler == nullptr)
+			EcoThrow << name << " pop_async message handler fail.";
+		return handler;
+	}
+
 	// add async message handler.
-	inline uint32_t add_async(eco::net::MessageHandler::ptr& hdl)
+	inline uint32_t add_async(MessageHandler::ptr& hdl)
 	{
 		uint32_t req_id = next_request_id();
 		m_async_map.set(req_id, hdl);
 		return req_id;
 	}
 
-	// pop async message handler.(get and delete)
-	inline eco::net::MessageHandler::ptr pop_async(
-		IN const uint32_t req_id, IN const bool last = true)
-	{
-		int eid = 0;
-		return last ? m_async_map.pop(req_id, eid) : m_async_map.get(req_id);
-	}
-
 private:
 	eco::Atomic<uint32_t> m_request_id;
-	eco::HashMap<uint32_t, eco::net::MessageHandler::ptr> m_async_map;
+	eco::HashMap<uint32_t, MessageHandler::ptr> m_async_map;
 };
 
 
