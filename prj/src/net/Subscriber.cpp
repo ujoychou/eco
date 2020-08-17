c9617c8125412ecce47c362fc4f32c2986e4993f
ujoychou <ujoychou@live.com>
2020-08-17 15:27:16 +0800

    get publisher by id, reset it's priority.

--- prj/src/net/Subscriber.cpp
+++ prj/src/net/Subscriber.cpp
@@ -7,27 +7,9 @@
 namespace eco{;
 namespace net{;
 ////////////////////////////////////////////////////////////////////////////////
-eco::Map<TopicUid, Publisher::ptr> g_sub_map;
+// dispatcher mode thread safe.
 std::map<TopicUid, Publisher::ptr> g_map;
 std::map<uint32_t, Publisher::ptr> g_typeid_map;
-////////////////////////////////////////////////////////////////////////////////
-void TopicUidMap::sub(IN const TopicUid& id, IN Publisher::ptr& pub)
-{
-	g_sub_map.set(id, pub);
-}
-void TopicUidMap::erase(IN const TopicUid& id)
-{
-	g_sub_map.erase(id);
-}
-Publisher::ptr TopicUidMap::get(IN const TopicUid& id)
-{
-	Publisher::ptr sub;
-	g_sub_map.find(sub, id);
-	return sub;
-}
-
-
-////////////////////////////////////////////////////////////////////////////////
 void TopicUidMap::set(IN uint32_t id, IN Publisher::ptr& pub)
 {
 	g_typeid_map[id] = pub;
@@ -36,23 +18,45 @@ void TopicUidMap::set(IN const TopicUid& id, IN Publisher::ptr& pub)
 {
 	g_map[id] = pub;
 }
+
+////////////////////////////////////////////////////////////////////////////////
+// subscriber mode thread unsafe.
+eco::Map<TopicUid, Publisher::ptr> g_sub_map;
+void TopicUidMap::sub(IN const TopicUid& id, IN Publisher::ptr& pub)
+{
+	g_sub_map.set(id, pub);
+}
+void TopicUidMap::erase(IN const TopicUid& id)
+{
+	g_sub_map.erase(id);
+}
+
+
+////////////////////////////////////////////////////////////////////////////////
 Publisher::ptr TopicUidMap::get(IN const TopicUid& id, IN uint32_t type_id)
 {
-	// type id map.
-	auto itt = g_typeid_map.find(type_id);
-	if (itt != g_typeid_map.end())
+	// priority: g_sub_map > g_map > g_typeid_map.
+	// #1.topic ids.
+	Publisher::ptr sub_;
+	if (g_sub_map.find(sub_, id))
 	{
-		return itt->second;
+		return sub_;
 	}
 
-	// topic uid map.
+	// #2.topic uid map.
 	auto itm = g_map.find(id);
 	if (itm != g_map.end())
 	{
 		return itm->second;
 	}
 
-	return nullptr;
+	// #3.type id map.
+	auto itt = g_typeid_map.find(type_id);
+	if (itt != g_typeid_map.end())
+	{
+		return itt->second;
+	}
+	return sub_;
 }
 
 
