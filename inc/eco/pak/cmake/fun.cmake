e642b58655ab9efce27831506bb7a98cc735f081
zhouyu <zhouyu@kernelsoft.com>
2024-10-14 15:25:51 +0800

    cmake update

--- inc/eco/pak/cmake/fun.cmake
+++ inc/eco/pak/cmake/fun.cmake
@@ -46,6 +46,32 @@ set(${__SRC_FILES__} ${_SRC_FILES_} PARENT_SCOPE)
 endfunction()
 
 # ---------------------------------------------------------------------
+# exclude c++ source files in 'SRC_DIRS_EXCLUDE'
+function(eco_project __PRJ_OUTPUT__ PRJ_FMT INSTALL_DIR)
+set(_PRJ_OUTPUT_ )
+if(${PRJ_FMT} STREQUAL "exe")
+    add_executable(${PROJECT_NAME})
+    set_target_properties(${PROJECT_NAME} 
+        PROPERTIES ARCHIVE_OUTPUT_DIRECTORY ${INSTALL_DIR})
+    set(_PRJ_OUTPUT_ ${INSTALL_DIR}/${PROJECT_NAME})
+elseif(${PRJ_FMT} STREQUAL "shared")
+    add_library(${PROJECT_NAME} SHARED)
+    set(_SUFFIX_ ".so.${PROJECT_VERSION}")
+    set_target_properties(${PROJECT_NAME} 
+        PROPERTIES LIBRARY_OUTPUT_DIRECTORY ${INSTALL_DIR})
+    set_target_properties(${PROJECT_NAME} PROPERTIES PREFIX "lib")
+    set_target_properties(${PROJECT_NAME} PROPERTIES SUFFIX ${_SUFFIX_})
+    set(_PRJ_OUTPUT_ ${INSTALL_DIR}/${PROJECT_NAME}${_SUFFIX_})
+elseif(${PRJ_FMT} STREQUAL "static")
+    add_library(${PROJECT_NAME} STATIC)
+    set_target_properties(${PROJECT_NAME} 
+        PROPERTIES RUNTIME_OUTPUT_DIRECTORY ${INSTALL_DIR})
+    set(_PRJ_OUTPUT_ ${INSTALL_DIR}/${PROJECT_NAME})
+endif()
+set(${__PRJ_OUTPUT__} ${_PRJ_OUTPUT_} PARENT_SCOPE)
+endfunction()
+
+# ---------------------------------------------------------------------
 # get absolute path of 'FILES_DIRS'
 function(eco_messages TITLE)
 foreach(_MSG_ ${ARGN})
